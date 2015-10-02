fa65f1332dd589a993b1d366a5d8af5824162424
saurabh7820 <saurabh7820@gmail.com>
2015-10-02 13:44:43 +0530

    Made some changes

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,7 @@
  * Created by saurabh on 02-Oct-15.
  */
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello, Saurabh!");
+    public static void main (String[] args){
+        System.out.println("Hello, Suarabh!");
     }
 }
